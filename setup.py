d39061cecd6f473da5377838c1d14178c7147a10
Philipp Schlegel <observing@web.de>
2024-04-16 20:58:07 +0100

    remove license classifier

--- setup.py
+++ setup.py
@@ -40,7 +40,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
